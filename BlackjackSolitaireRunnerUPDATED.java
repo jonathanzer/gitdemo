c8e5b547d328c38fa2d9538afd8ddd17ae4a93e8
john <zerai@seas.upenn.edu>
2019-06-23 18:01:43 +0200

    changed printout back again

--- BlackjackSolitaireRunnerUPDATED.java
+++ BlackjackSolitaireRunnerUPDATED.java
@@ -3,6 +3,7 @@ public class BlackjackSolitaireRunner {
 	
 	public static void main(String[] args) {
 		BlackjackSolitaire bjs = new BlackjackSolitaire();
-		System.out.println("deleted the call to the play method.");
+		System.out.println("Changed print out a second time");
+
 	}
 }
